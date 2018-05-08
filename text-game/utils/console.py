d793ada6a6eb745e02c22d167ef3df4be6e04a74
pathdll <pcampaa@gmail.com>
2018-05-08 00:25:52 -0300

    console style progress

--- text-game/utils/console.py
+++ text-game/utils/console.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import time
 from shutil import get_terminal_size
 from colorama import init
 from termcolor import colored
@@ -17,36 +18,43 @@ class Console:
     self.update_res()
     return self._res
 
-  def set_pos(self, x_pos, y_pos):
+  def _register_move(self, x_pos, y_pos):
     self.last_pos = self.pos
     self.pos = (x_pos, y_pos)
 
   def update_res(self):
     self._res = get_terminal_size()
   
-  def write(self, msg, color=None, on_color=None, limit_x=True):
+  def write(self, msg, color=None, on_color=None):
     msg = str(msg)
     sys.stdout.write(colored(msg, color=color, on_color=on_color))
-    self.set_pos(self.pos[0] + len(msg), self.pos[1])
-    # TODO: Limit horizontal write
-    width, height = self.get_res()
+    sys.stdout.flush()
 
+    width = self.get_res()[0]
+    if self.pos[0] + len(msg) > width:
+      self.move(width-1, self.pos[1])
+    else:
+      self._register_move(self.pos[0] + len(msg), self.pos[1])
   
   def _esc_write(self, msg):
     sys.stdout.write("\x1b[%s" % (msg))
+    sys.stdout.flush()
 
   def clear(self):
     self._esc_write('2J')
-    self.set_pos(1, 1)
+    self._register_move(0, 0)
   
   def read(self):
     x = input()
     self.move(self.pos[0], self.pos[1])
     return x
 
+  # coors in box: (0, 0) x (width-1, height-1)
   def move(self, x, y):
+    x = x + 1
+    y = y + 1
     self._esc_write('%d;%dH' % (y, x))
-    self.set_pos(x, y)
+    self._register_move(x, y)
 
   def rel_move(self, x, y):
     if x > 0:
@@ -57,18 +65,22 @@ class Console:
       self._esc_write('%dA' % (y))
     elif y < 0:
       self._esc_write('%dB' % (-y))
-    self.set_pos(self.pos[0] + x, self.pos[1] + y)
+    self._register_move(self.pos[0] + x, self.pos[1] + y)
 
   def go_back(self):
-    self.set_pos(*self.last_pos)
+    self._register_move(*self.last_pos)
 
   def hprint(self, msg, rep=1, step=1):
     for _ in range(rep):
       self.write(msg)
-      self.rel_move(step-1, 0)
+      if step > 0:
+        self.rel_move(step-1, 0)
+      elif step < 0:
+        self.rel_move(step-1, 0)
   
   def vprint(self, msg, rep=1, step=1):
     for _ in range(rep):
       self.write(msg)
-      self.go_back()
-      self.rel_move(0, step)
+      if step > 0:
+        self.rel_move(-1, step)
+
--- text-game/utils/style.py
+++ text-game/utils/style.py
@@ -15,18 +15,31 @@ def move(x, y):
 def rel_move(x, y):
   cmd.rel_move(x, y)
 
-def draw_border():
-  width, height = cmd.get_res()
+def draw_box(x0, y0, x1, y1):
+  sw, sh = cmd.get_res()
+  if y1 < 0 or y1 < 0 or y1 > sw or y1 > sh:
+    print("[ERROR] box outside limits")
+    return
+  if y1 <= y0 or x1 <= x0:
+    print("[ERROR] box coors inconsistent")
+    return
 
-  cmd.clear()
+  width = x1 - x0
+  height = y1 - y0
+
+  cmd.move(x0, y0)
   cmd.hprint("*", width-1)
   time.sleep(1)
   cmd.vprint("*", height-2, -1)
   time.sleep(1)
   cmd.hprint("*", width-1, -1)
-  cmd.rel_move(-1, 0)
   time.sleep(1)
-  cmd.vprint("*", height-1, 1)
+  cmd.vprint("*", height-1)
+
+def draw_border():
+  width, height = cmd.get_res()
+  cmd.clear()
+  draw_box(0, 0, width, height)
 
 def get_cursor():
   return cmd.pos
